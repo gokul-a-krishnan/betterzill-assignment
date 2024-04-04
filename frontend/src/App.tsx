74b7a9da74fb60a2f614a6c6a88bb18b6654977f
Gokulakrishnan <gokulakrishnan.new@gmail.com>
2024-04-04 13:58:53 +0530

    tested frondend and backend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,82 +1,154 @@
-import { useState } from "react";
-// import "./App.css";
+import React, { useState } from "react";
+import "./App.css";
 
-export default function App() {
-  const [result, setResult] = useState();
-  const [question, setQuestion] = useState();
-  const [file, setFile] = useState();
+import PdfIcon from "./asset/pdf.png";
+import DocIcon from "./asset/doc.png";
+import CsvIcon from "./asset/csv.png";
+import TxtIcon from "./asset/txt.png";
+import logo from "./asset/logo.svg";
+import axios from "axios";
 
-  const handleQuestionChange = (event: any) => {
-    setQuestion(event.target.value);
+function App() {
+  const [isUploaded, setIsUploaded] = useState(false);
+  const [iconData, setIconData] = useState({
+    name: "",
+    type: "",
+    secretname: "",
+  });
+  const [chatHistory, setChatHistory] = useState<
+    { role: string; message: string }[]
+  >([]);
+  const [inputText, setInputText] = useState("");
+
+  const icons: { [id: string]: string } = {
+    pdf: PdfIcon,
+    doc: DocIcon,
+    docx: DocIcon,
+    csv: CsvIcon,
+    txt: TxtIcon,
   };
 
-  const handleFileChange = (event: any) => {
-    setFile(event.target.files[0]);
-  };
+  const handleOnFileSelected = async (e: any) => {
+    e.preventDefault();
 
-  const handleSubmit = (event: any) => {
-    event.preventDefault();
+    const file = e.target.files[0];
+    if (file == null) {
+      setIsUploaded(false);
+      return;
+    }
 
     const formData = new FormData();
+    formData.append("file", file);
 
-    if (file) {
-      formData.append("file", file);
-    }
-    if (question) {
-      formData.append("question", question);
-    }
+    const res = await axios.post("http://localhost:8000/upload", formData, {
+      headers: { "Content-Type": "multipart/form-data" },
+    });
 
-    fetch("http://127.0.0.1:8000/predict", {
-      method: "POST",
-      body: formData,
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        setResult(data.result);
-      })
-      .catch((error) => {
-        console.error("Error", error);
-      });
+    const { filetype, filename } = res.data;
+
+    setIconData({
+      name: file.name,
+      type: filetype,
+      secretname: filename,
+    });
+
+    setIsUploaded(true);
+  };
+
+  const handleSubmit = async (e: any) => {
+    e.preventDefault();
+
+    const response = await axios.post("http://localhost:8000/predict", {
+      query: inputText,
+      filename: iconData.secretname,
+    });
+
+    setChatHistory([
+      ...chatHistory,
+      {
+        role: "user",
+        message: inputText,
+      },
+      {
+        role: "ai",
+        message: response.data.result,
+      },
+    ]);
+    setInputText("");
   };
 
   return (
-    <div className="appBlock">
-      <form onSubmit={handleSubmit} className="form">
-        <label className="questionLabel" htmlFor="question">
-          Question:
-        </label>
-        <input
-          className="questionInput"
-          id="question"
-          type="text"
-          value={question}
-          onChange={handleQuestionChange}
-          placeholder="Ask your question here"
-        />
+    <div className="App">
+      <aside className="sidebar">
+        <div className="menu-title">Upload file</div>
+        {isUploaded ? (
+          <></>
+        ) : (
+          <div className="upload-button">
+            <input
+              type="file"
+              placeholder="x"
+              id="file"
+              name="file"
+              accept=".csv, .pdf, .doc, .docx, .txt"
+              className="fileInput"
+              onChange={handleOnFileSelected}
+            />
+          </div>
+        )}
 
-        <br></br>
-        <label className="fileLabel" htmlFor="file">
-          Upload CSV file:
-        </label>
+        {isUploaded ? (
+          <div className="file-preview">
+            <img
+              className="file-icon"
+              src={icons[iconData.type]}
+              alt={iconData.type + " icon"}
+            />
+            {iconData.name}
+          </div>
+        ) : (
+          <></>
+        )}
+      </aside>
+      <section className="chatbox">
+        {isUploaded ? (
+          <>
+            <div className="message-list">
+              {chatHistory.map((chat, index) => (
+                <div key={index} className={`chat-message ${chat.role}`}>
+                  <div className="chat-message-center">
+                    <div className={`avatar ${chat.role}`}>
+                      {chat.role === "ai" ? (
+                        <img src={logo} alt="logo" />
+                      ) : (
+                        <></>
+                      )}
+                    </div>
+                    <div className="message">{chat.message}</div>
+                  </div>
+                </div>
+              ))}
+            </div>
 
-        <input
-          type="file"
-          id="file"
-          name="file"
-          accept=".csv"
-          onChange={handleFileChange}
-          className="fileInput"
-        />
-        <br></br>
-        <button
-          className="submitBtn"
-          type="submit"
-          disabled={!file || !question}
-        >
-          Submit
-        </button>
-      </form>
-      <p className="resultOutput">Result: {result}</p>
+            <div className="user-input-holder">
+              <form onSubmit={handleSubmit}>
+                <input
+                  placeholder="Start typing..."
+                  className="user-input-textarea"
+                  value={inputText}
+                  onChange={(e) => {
+                    setInputText(e.target.value);
+                  }}
+                ></input>
+              </form>
+            </div>
+          </>
+        ) : (
+          <center>Upload a file to start chat</center>
+        )}
+      </section>
     </div>
   );
 }
+
+export default App;
